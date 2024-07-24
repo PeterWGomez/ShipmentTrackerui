b246218abecf8b5fa7a28659cdc9baa3e4c78e33
PeterWGomez <petergomezcs@gmail.com>
2024-07-23 22:00:18 -0600

    adjusted one unit test

--- src/main/kotlin/ExpressShipment.kt
+++ src/main/kotlin/ExpressShipment.kt
@@ -1,6 +1,5 @@
 interface ExpressShipment {
     fun checkExpress(updatetime: Long, createdtime: Long): Boolean {
-        println("${updatetime} - ${createdtime} = ${updatetime - createdtime}")
         if (updatetime - createdtime < 259200000) {
             println("Here")
             return true
